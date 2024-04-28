bfef954a9000884f68e15b80966f7530ef1b6764
Tobias Hüske <t.hueske@mittwald.de>
2024-04-28 20:30:03 +0200

    Changes

--- conf/wp/wp-config.php
+++ conf/wp/wp-config.php
@@ -1,15 +1,15 @@
 <?php
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
+define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'db') );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
 
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
     $_SERVER['HTTPS'] = 'on';
