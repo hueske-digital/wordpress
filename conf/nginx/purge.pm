909a0ae7f268bc61b8ded25bbabb26fb88918ee8
Tobias Hüske <t.hueske@mittwald.de>
2024-06-30 07:14:12 +0200

    change order

--- conf/nginx/purge.pm
+++ conf/nginx/purge.pm
@@ -1,9 +1,22 @@
 package purge;
 
+# Füge dynamisch den Architektur-spezifischen Pfad hinzu
+use Config;
+use File::Spec;
+
+BEGIN {
+    my $archname = $Config{archname};
+    my $vendorlib = File::Spec->catdir($Config{vendorlibexp}, $archname);
+    push @INC, $vendorlib;
+}
+
 use nginx;
 
 sub purge {
+    # Ein Systembefehl wird ausgeführt, um den Cache zu leeren
     system "rm -rf /etc/nginx/cache/*";
+
+    # HTTP_NO_CONTENT ist eine Konstante aus dem Nginx-Modul und bedeutet 204 No Content
     return HTTP_NO_CONTENT;
 }
 
