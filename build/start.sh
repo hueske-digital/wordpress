3479cb1723f762640b45adc1481048cb3e350fc8
Tobias Hüske <t.hueske@mittwald.de>
2025-04-14 21:26:14 +0200

    Use watchexec

--- build/start.sh
+++ build/start.sh
@@ -3,16 +3,12 @@
 echo "Running gomplate to generate /etc/msmtprc..."
 gomplate -f /etc/msmtprc.tmpl -o /etc/msmtprc
 
-echo "Starting iniwatch to monitor /usr/local/etc/php/conf.d..."
-(
-    WATCH_DIR="/usr/local/etc/php/conf.d"
-    echo "Watching for changes in $WATCH_DIR..."
-    while inotifywait -e modify,create,delete,move "$WATCH_DIR"; do
-        echo "Change detected! Reloading php-fpm..."
-        pkill -o -USR2 php-fpm
-        echo "Done"
-    done
-) &
+echo "Starting watchexec to monitor /usr/local/etc/php/conf.d/zzzz-user.ini..."
+watchexec -p -w /usr/local/etc/php/conf.d/zzzz-user.ini -d 250ms "
+  echo 'Change detected! Reloading php-fpm...'
+  pkill -o -USR2 php-fpm
+  echo 'Done'
+" &
 
 echo "Starting php-fpm..."
 exec php-fpm
